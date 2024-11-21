b001062cda288c91bdd3ffb081f2cbcd61957f20
Fran Montecinos <ernestozaratejm@gmail.com>
2024-11-21 00:24:07 -0400

    baccccccccck

---
+++ src/config.js
@@ -0,0 +1,10 @@
+import {config} from "dotenv";
+
+config();
+
+export const PORT = 4000;
+export const DBPORT = "3306";
+export const HOST = "127.0.0.1";
+export const DATABASE = "clinica";
+export const DBUSER = "root";
+export const PASSWORD = "";
